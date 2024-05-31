8151193899ac511a81ed82b23abe952f60e3071e
grey <iamwotou@gmail.com>
2024-05-31 18:02:53 -0500

    remove some hard coded magic numbers

--- src/Motifs.jsx
+++ src/Motifs.jsx
@@ -1,12 +1,15 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import Plot from 'react-plotly.js';
 import * as ort from 'onnxruntime-web';
 
 const Motifs = () => {
     const [output, setOutput] = useState(null);
+    const scrollContainerRef = useRef(null);
+
+    const halfLen = 2000
 
     const fetchSeq = async () => {
-        const res2 = await fetch(`https://api.genome.ucsc.edu/getData/sequence?genome=hg38;chrom=chr7;start=${5530600 - 2000 - 1};end=${5530600 + 2000}`);
+        const res2 = await fetch(`https://api.genome.ucsc.edu/getData/sequence?genome=hg38;chrom=chr7;start=${5531000 - halfLen - 1 - 14};end=${5531000 + halfLen + 14}`); // padd 14 on each to make output the same as seq len
         const r2 = await res2.json();
         const sequence = r2.dna.toUpperCase();
         return sequence
@@ -20,19 +23,24 @@ const Motifs = () => {
                 // Prepare the input data
                 const inputSequence = await fetchSeq();
                 const inputArray = oneHotEncode(inputSequence);
-                const tensor = new ort.Tensor('float32', inputArray, [1, 4, 4001]); // Adjust the shape if needed
+                const tensor = new ort.Tensor('float32', inputArray, [1, 4, halfLen * 2 + 29]); // Hard coded dimension for now
                 // Run the model
                 const feeds = { input: tensor };
                 const results = await session.run(feeds);
-                // Get the output
                 const outputData = results.output.data;
                 setOutput(outputData);
-            } catch (err) {
-                console.error(err);
-            }
+            } catch (err) { console.error(err); }
         };
         runModel();
     }, []);
+
+    useEffect(() => {
+        if (scrollContainerRef.current) {
+            // Set scroll position to the middle (50%) after the output is set
+            scrollContainerRef.current.scrollLeft = scrollContainerRef.current.scrollWidth / 2 - scrollContainerRef.current.clientWidth / 2;
+        }
+    }, [output]);
+
     const oneHotEncode = (seq) => {
         const mapping = { 'A': 0, 'C': 1, 'G': 2, 'T': 3 };
         const indices = Array.from(seq).map(char => mapping[char]);
@@ -46,45 +54,34 @@ const Motifs = () => {
     // Function to plot all motifs
     const plotAllMotifs = () => {
         if (!output) return null;
-        // Number of data points per motif
-        const pointsPerMotif = 3973;
-        // Total number of motifs
+        const pointsPerMotif = halfLen * 2 + 1;
         const numMotifs = 243;
-        // Create traces for each motif
         const data = [];
-        for (let i = 0; i < 50; i++) { // Adjust the number of motifs to avoid crashing
-            // const motif = output.slice(i * pointsPerMotif, (i + 1) * pointsPerMotif);
-            const motif = output.slice(i * pointsPerMotif, (i + 1) * pointsPerMotif).map(value => parseFloat(value.toFixed(1)));
 
+        for (let i = 0; i < numMotifs; i++) { 
+            // reduce precision to 2 digit after decimal
+            const motif = output.slice(i * pointsPerMotif, (i + 1) * pointsPerMotif).map(value => parseFloat(value.toFixed(2)));
+            const allZero = motif.every(value => value === 0);
+            if (allZero) continue; // Skip this motif if all values are zero
             data.push({
                 x: Array.from({ length: pointsPerMotif }, (_, j) => j + 1),
                 y: motif,
                 type: 'scatter',
-                mode: 'lines+markers',
-                name: `Motif ${i + 1}`, // Optional: assign a name to each motif
+                mode: 'lines',
+                name: `Motif ${i + 1}`, 
             });
         }
+
         return (
-            <div style={{ overflowX: 'scroll', width: '1000px' }}>
-                <div style={{ width: '40000px' }}>
-                    <Plot
-                        data={data}
-                        layout={{
-                            title: 'All Motifs Plot',
-                            width: 40000, // Set plot width to 40000px
-                            xaxis: {
-                                range: [0, pointsPerMotif], // Set initial x-axis range
-                            },
-                        }}
-                        useResizeHandler={true}
-                        style={{ width: '100%', height: '600px' }} // Adjust height as needed
+            <div ref={scrollContainerRef}  style={{ overflowX: 'scroll', width: '1000px' }}>
+                <div style={{ width: `${halfLen*20}px` }}>
+                    <Plot data={data} useResizeHandler={true} style={{ width: '100%', height: '300px' }} 
+                        layout={{ showlegend:false, }}
                     />
                 </div>
             </div>
         );
     };
-
-
 
     return (
         <div>
